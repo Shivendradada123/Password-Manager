7f724508b5c0064be1eded15b67c8b64bfce8a2b
Shivedra123 <shivendrasaket2580@gmail.com>
2025-12-18 20:01:46 +0530

    toast

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,31 +5,37 @@ const Nav = ({ search, setSearch }) => {
   return (
     <nav className="w-full bg-gray-900 border-b border-gray-700">
       <div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between text-white">
-        <h1 className="text-2xl font-bold tracking-wide">
-          Password<span className="text-blue-400">Manager</span>
+        <h1 className="text-3xl font-bold tracking-wide text-white ">
+          Password Manager
         </h1>
 
-        <div className="flex items-center gap-4">
-          <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            type="text"
-            placeholder="Search password"
-            className="px-4 py-1.5 rounded-full bg-gray-800 border border-gray-600 text-sm outline-none focus:ring-2 focus:ring-blue-500"
-          />
+        <div className="flex items-center    gap-8">
+       <input
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+  onKeyUp={(e) => {
+    if (e.key === "Enter") {
+      console.log("Search value:", e.target.value);
+    }
+  }}
+  type="text"
+  placeholder="Search password"
+  className="px-4 py-1.5 rounded-full bg-gray-800 border border-gray-600 text-sm outline-none focus:ring-2 focus:ring-blue-500"
+/>
 
-          <button className="px-4 py-1.5 rounded-full border border-blue-500 text-blue-400 hover:bg-blue-500 hover:text-white transition">
+
+          <button className="px-4 py-1.5 rounded-full border active:scale-95 border-blue-500 text-blue-400 hover:bg-blue-500 hover:text-white transition">
             Search
           </button>
 
           <Link to="/">
-            <button className="px-4 py-1.5 rounded-full border border-gray-500 hover:bg-white hover:text-black transition">
+            <button className="px-4 py-1.5 rounded-full active:scale-95 border border-gray-500 hover:bg-white hover:text-black transition">
               Sign in
             </button>
           </Link>
 
           <Link to="/signup">
-            <button className="px-4 py-1.5 rounded-full bg-blue-500 text-white hover:bg-blue-600 transition">
+            <button className="px-4 py-1.5 active:scale-95  rounded-full bg-blue-500 text-white hover:bg-blue-600 transition">
               Sign up
             </button>
           </Link>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,73 +2,52 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signuppage = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [toast, setToast] = useState("")
+  const navigate = useNavigate();
+
+  // signup btn ke liye  fnc
+  const handlePage = () => {
+    if (!email || !password) {
+      showToast("Please fill all details")
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      showToast("Please enter a valid email address")
+      return;
+    }
+
+    // Data get karane ke lie localstorage se
+
+    const users = JSON.parse(localStorage.getItem("Users")) || [];
+
+    // agar user hai to check karne ke liye
+    const exist = users.some((u) => u.email === email);
+    if (exist) {
+      showToast("user is already ragister")
+      return;
+    }
+
+    const newUser = { email, password };
+
+    users.push(newUser);
+    localStorage.setItem("Users", JSON.stringify(users));
+    showToast("Signup success")
+    navigate("/");
+  };
+
+  const showToast =(msg)=>{
+
+    setToast(msg)
+     setTimeout(() => {
+      setToast("")
+     }, 3000);
 
 
-
-        const [email, setemail] = useState("");
-        const [password, setpassword] = useState("");
-
-              const navigate = useNavigate()
-
-
-        // signup btn ke liye  fnc
-        const handlePage=()=>{
-          if(!email || !password){
-
-
-            alert("Please fill all details")
-            return
-          };
-
-
-
-
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    alert("Please enter a valid email address");
-    return;
   }
-
-          // Data get karane ke lie localstorage se 
-
-          const users = JSON.parse(localStorage.getItem("Users")) || []
-
-
-            // agar user hai to check karne ke liye
-            const exist = users.some((u)=>u.email === email)
-          if(exist){
-            alert("user is already ragister");
-            return
-          };
-
-          
-
-          const newUser = {email, password};
-
-          users.push(newUser); 
-          localStorage.setItem("Users", JSON.stringify(users));
-          alert("signup success")
-          navigate("/")
-  
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return (
     <div className="min-h-screen flex justify-center items-center ">
@@ -87,7 +66,7 @@ const Signuppage = () => {
         <input
           required
           type="email"
-          onChange={(e)=> setemail(e.target.value)}
+          onChange={(e) => setemail(e.target.value)}
           placeholder=" Eg - Shivnedrapulser09@gmail.com"
           className="w-full bg-black/20 mt-2 p-3 border border-gray-600 rounded-xl text-white mb-6"
         />
@@ -96,18 +75,23 @@ const Signuppage = () => {
         <input
           required
           type="password"
-          onChange={(e)=> setpassword(e.target.value)}
+          onChange={(e) => setpassword(e.target.value)}
           placeholder=" Eg - Shivendra@#$#1232"
           className="w-full bg-black/20 mt-2 p-3 border border-gray-600 rounded-xl text-white mb-6"
         />
 
-        
-          <button onClick={handlePage} className="w-full py-3  active:scale-95   bg-white text-black font-medium rounded-2xl">
-            SignUp
-          </button>
-      
+        <button
+          onClick={handlePage}
+          className="w-full py-3  active:scale-95   bg-white text-black font-medium rounded-2xl"
+        >
+          SignUp
+        </button>
+        {toast && (
+  <div className="fixed top-5 right-5 bg-red-600  text-white px-6 py-3 rounded-xl border border-gray-600 shadow-lg animate-pulse">
+    {toast}
+  </div>
+)}
 
-        
       </div>
     </div>
   );
